c9bd2cebec615c8c924f415ca9a0ec7a36d9b7b8
kor-taeyoon <dbsl0512@gmail.com>
2021-03-01 15:23:06 +0900

    gps test macro added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 
+//#define test
+
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
@@ -13,17 +15,29 @@ float current_lng;
 long gps_last_updated = 0;
 
 void Debugger(){
-    Serial.print("!");
-    Serial.print(current_lat, 6);
-    Serial.print("*");
-    Serial.print(current_lng, 6);
-    Serial.print("#");
+    #ifdef test
+        Serial.print("!");
+        Serial.print("37.123456");
+        Serial.print("*");
+        Serial.print("127.654321");
+        Serial.print("#");
+    #else
+        Serial.print("!");
+        Serial.print(current_lat, 6);
+        Serial.print("*");
+        Serial.print(current_lng, 6);
+        Serial.print("#");
+    #endif
 }
 
 void setup() {
     // UART setup
     Serial.begin(9600);
     GpsSerial.begin(9600);
+
+    // pinmode setup
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 
     // timer scheduleing setup
     timer.setInterval(1000, Debugger);
@@ -41,5 +55,13 @@ void loop() {
         current_lat = gps.location.lat();
         current_lng = gps.location.lng();
         gps_last_updated = millis();
+    }
+
+    // GPS state show
+    if(current_lat != 0){
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else{
+        digitalWrite(LED_BUILTIN, LOW);
     }
 }
